5d27d3b16d4ec987f0a00d9c1488218b383dd37c
Katie Wessman <kjeanne@gmail.com>
2024-12-02 18:05:19 -0600

    Restructure app to follow MVC archtiecture

---
+++ src/js/helpers.js
@@ -0,0 +1,24 @@
+import { TIMEOUT_SEC } from './config';
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
+export const getJSON = async function (url) {
+  try {
+    // Retrieve the data using API
+    const res = await Promise.race([fetch(url), timeout(TIMEOUT_SEC)]);
+    // Convert data to readable format
+    const data = await res.json();
+
+    // If the data doesn't load pass an error
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    return data;
+  } catch (err) {
+    throw err;
+  }
+};
